e5bc35dd193d980685255301d156da8abcf81a73
naracodes <naralee0124@gmail.com>
2020-12-09 13:16:46 -0500

    add basic variables

---
+++ src/main/java/com/sparta/week01/prac/Prac.java
@@ -0,0 +1,11 @@
+package com.sparta.week01.prac;
+
+public class Prac {
+    public static void main(String[] args) {
+//        System.out.println("Hello!");
+        String myName = "Nara";
+        float fNum = 2.4f;
+        Long largeNum = 1000000000000L;
+        System.out.println(myName);
+    }
+}
