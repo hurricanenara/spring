298fa00e1df54f480df22543505a4af2c3598045
naracodes <naralee0124@gmail.com>
2020-12-09 14:00:23 -0500

    add array list

--- src/main/java/com/sparta/week01/prac/Prac.java
+++ src/main/java/com/sparta/week01/prac/Prac.java
@@ -1,11 +1,40 @@
 package com.sparta.week01.prac;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Prac {
     public static void main(String[] args) {
-//        System.out.println("Hello!");
+/*
+        System.out.println("Hello!");
         String myName = "Nara";
         float fNum = 2.4f;
         Long largeNum = 1000000000000L;
         System.out.println(myName);
+*/
+/*
+        int myAge = 29;
+        boolean isAdult = myAge > 20;
+        System.out.println(isAdult);
+        System.out.println(myAge == 21);
+*/
+/*
+        List<String> myList = new ArrayList<>();
+        String course1 = "Java Spring";
+        String course2 = "React";
+        myList.add(course1);
+        myList.add(course2);
+        System.out.println(myList);
+        System.out.println(myList.get(1));
+        myList.remove(1);
+        System.out.println(myList);
+*/
+
+        List<String> courseList = new ArrayList<>();
+        String course1 = "Web Dev";
+        String course2 = "App Dev";
+        courseList.add(course1);
+        courseList.add(course2);
+        System.out.println(courseList);
     }
 }
