c276e00d87668501383b4d1b8ccceada97c6cc36
naracodes <naralee0124@gmail.com>
2020-12-11 22:08:09 -0500

    add java class

---
+++ src/main/java/com/sparta/week01/prac/Course.java
@@ -0,0 +1,8 @@
+package com.sparta.week01.prac;
+
+public class Course {
+    // title, tutor, days 가 Course 라는 맥락 아래에서 의도가 분명히 드러나죠!
+    public String title;
+    public String tutor;
+    public int days;
+}
